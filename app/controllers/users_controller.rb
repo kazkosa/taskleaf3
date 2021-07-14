a2b4787840d3c278a4384740d04b550477de26a3
kazkosa <kazu_kosa@icloud.com>
2021-07-15 06:20:31 +0900

    create basic-login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
     @user = User.new 
   end
   def create
-    # binding.pry
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Task Leaf!"
       redirect_to @user
     else
