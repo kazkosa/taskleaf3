b792186b90972d138a4434bb18972f6b030da65a
kazkosa <kazu_kosa@icloud.com>
2021-07-18 06:35:37 +0900

    implement adminuser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Example User",
+  email: "example@mail.com",
+  password:              "foobar",
+  password_confirmation: "foobar",
+  admin: true)
+
+5.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@mail.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password
+  )
+end
